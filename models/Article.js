8b1d9cba47839979c6a03d91233e4c7fb9fecfb7
qbou99 <qbou@outlook.fr>
2022-03-08 19:00:37 +0100

    Ajout description et types

--- models/Article.js
+++ models/Article.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 const articleSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true, minlength: 2, unique: true },
   price: { type: Number, required: true, min: 0 },
+  description: { type: String, trim: true },
+  types: [String],
   image: String,
 });
 
